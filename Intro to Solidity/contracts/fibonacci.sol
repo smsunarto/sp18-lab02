bc1215a2d1bdeb1c1b7f396540c18be705183c97
Scott M Sunarto <smsunarto@users.noreply.github.com>
2018-06-28 21:08:53 +0700

    Implement fibonacci.sol

--- Intro to Solidity/contracts/fibonacci.sol
+++ Intro to Solidity/contracts/fibonacci.sol
@@ -4,9 +4,21 @@ pragma solidity 0.4.19;
 contract Fibonacci {
     /* Carry out calculations to find the nth Fibonacci number */
     function fibRecur(uint n) public pure returns (uint) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibRecur(n-1) + fibRecur(n-2);
     }
 
     /* Carry out calculations to find the nth Fibonacci number */
-    function fibIter(uint n) public returns (uint) {
+    function fibIter(uint n) public pure returns (uint) {
+        uint previous = 1;
+        uint current = 1;
+        for (uint i = 3; i <= n; i++) {
+            uint temp = current + previous;
+            previous = current;
+            current = temp;
+        }
+        return current;
     }
 }
